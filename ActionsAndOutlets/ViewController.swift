d63c96de721d821d4ff6b3eb2a4f8f80b60b4bb8
Omar Mendez <omendez815@gmail.com>
2020-04-19 14:26:12 -0500

    Initial Commit

---
+++ ActionsAndOutlets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActionsAndOutlets
+//
+//  Created by Omar Mendez on 4/19/20.
+//  Copyright © 2020 Omar Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
